b7023f0339779d753f418e044e1b4b55df29a6cd
文月文月吃八盤(?) <requiem.for.memories@gmail.com>
2016-05-05 03:11:21 +0000

    change blog title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   def index
     if params[:category]
       @articles = Article.where(category_id: params[:category]).order('created_at DESC')
+      @pagetitle = Category.find(params[:category]).name
     else
       @articles = Article.all.order('created_at DESC')
     end
@@ -12,6 +13,7 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+    @pagetitle = @article.title
   end
   
 
